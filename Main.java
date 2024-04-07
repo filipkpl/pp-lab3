6c720182ba0edf8487d8677f09261f439d1b19ee
filipkpl <filippoo841@gmail.com>
2024-04-07 12:59:21 +0200

    x

---
+++ Main.java
@@ -0,0 +1,10 @@
+import geometry.Point;
+
+public class Main {
+    public static void main(String[] args) {
+        // Point
+        Point center = new Point(0, 0);
+        System.out.println("zad1");
+        System.out.println("Właściwości punktu: " + center);
+    }
+}
