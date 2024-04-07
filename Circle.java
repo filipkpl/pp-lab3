03b59b36361edef2dd073e47a67829f8a57ebd1c
filipkpl <filippoo841@gmail.com>
2024-04-07 12:11:01 +0200

    x

---
+++ Circle.java
@@ -0,0 +1,31 @@
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Koło[środek=" + center + ", promień=" + radius + "]";
+    }
+}
+
---
+++ calculate.java
@@ -0,0 +1,21 @@
+public class calculate {
+    public static void main(String[] args) {
+
+        Circle[] circles = new Circle[] {
+            new Circle(new Point(0, 0), 5),
+            new ColoredCircle(new Point(1, 1), 7, "niebieski"),
+            new Circle(new Point(2, 2), 3),
+            new ColoredCircle(new Point(3, 3), 4, "zielony")
+        };
+
+        for (Circle circle : circles) {
+            System.out.println("Pole powierzchni koła: " + circle.calculateArea());
+            
+ 
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle;
+                System.out.println("Kolor koła: " + coloredCircle.getColor());
+            }
+        }
+    }
+}
